b2cee344ce97b0b71f1c4e62db4ff2c6c8926396
Chandler Hulstrom <chulstro11@gmail.com>
2020-07-01 17:15:17 -0600

    User story 2 complete

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "shelters index page", type: :feature do
     shelter_1 = Shelter.create(name: "Habitat for Hippos")
     shelter_2 = Shelter.create(name: "Loving Arms")
 
-    visit /shelters
+    visit '/shelters'
 
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_2.name)
