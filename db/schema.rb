fe78521a1f88614355e529388b1e03f7f040ac51
Chandler Hulstrom <chulstro11@gmail.com>
2020-07-04 11:08:22 -0600

    Pet main page completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200701225926) do
+ActiveRecord::Schema.define(version: 20200704165333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "image"
+    t.string "name"
+    t.integer "approx_age"
+    t.string "sex"
+    t.bigint "shelter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "shelters_id"
+    t.index ["shelters_id"], name: "index_pets_on_shelters_id"
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 20200701225926) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pets", "shelters", column: "shelters_id"
 end
