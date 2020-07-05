6d742f0ed1c8a2ea4d9b25527154bd7296b671bc
Chandler Hulstrom <chulstro11@gmail.com>
2020-07-05 08:59:14 -0600

    Edit button added to shelter index page

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Edit a shelter" do
-  it "I can edit a shelter's info" do
-    shelter_1 = Shelter.create(name: "Habitat for Hippos", address: "184 w Plaintiff rd", city: "Denver", state: "CO", zip: 80504)
+  before :each do
+    @shelter_1 = Shelter.create(name: "Habitat for Hippos", address: "184 w Plaintiff rd", city: "Denver", state: "CO", zip: 80504)
+  end
+
+  it "I can edit a shelter's info on a show page" do
 
     visit '/shelters'
 
@@ -12,7 +15,7 @@ RSpec.describe "Edit a shelter" do
 
     click_on "Update Shelter"
 
-    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
 
     fill_in :name, with: "Habitat for Hippos and Lemurs"
     fill_in :address, with: "184 w Plaintiff rd"
@@ -22,8 +25,17 @@ RSpec.describe "Edit a shelter" do
 
     click_on "Update Shelter"
 
-    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}")
     expect(page).to have_content("Habitat for Hippos and Lemurs")
 
   end
+
+  it "A link to the edit page is on every shelter in the index" do
+
+    visit '/shelters'
+
+    click_on "Edit #{@shelter_1.name}"
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
+  end
+
 end
