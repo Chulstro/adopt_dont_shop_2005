48ba7c3b56bb0eb5b7f3bb6ae50187a8a445ba83
Chandler Hulstrom <chulstro11@gmail.com>
2020-07-05 09:11:10 -0600

    Delete button for shelters added to index page

--- spec/features/shelters/delete_spec.rb
+++ spec/features/shelters/delete_spec.rb
@@ -1,11 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "When I visit a shelter show page" do
-  it "I can delete a shelter" do
-    shelter_1 = Shelter.create(name: "Habitat for Hippos", address: "184 w Plaintiff rd", city: "Denver", state: "CO", zip: 80504)
-    shelter_2 = Shelter.create(name: "Loving Arms", address: "234 Lugie rd", city: "Montrose", state: "AR", zip: 90876)
+  before :each do
+    @shelter_1 = Shelter.create(name: "Habitat for Hippos", address: "184 w Plaintiff rd", city: "Denver", state: "CO", zip: 80504)
+    @shelter_2 = Shelter.create(name: "Loving Arms", address: "234 Lugie rd", city: "Montrose", state: "AR", zip: 90876)
+  end
 
-    visit "/shelters/#{shelter_2.id}"
+  it "I can delete a shelter from a show page" do
+
+    visit "/shelters/#{@shelter_2.id}"
 
     expect(page).to have_content("Loving Arms")
 
@@ -13,5 +16,14 @@ RSpec.describe "When I visit a shelter show page" do
 
     expect(current_path).to eq("/shelters")
     expect(page).to_not have_content("Loving Arms")
+  end
+
+  it "Can access destroy a shelter from the indew page" do
+
+    visit '/shelters'
+
+    click_on "Delete #{@shelter_1.name}"
+    expect(current_path).to eq('/shelters')
+    expect(page).to_not have_content("Habitat for Hippos")
   end
 end
